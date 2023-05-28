ec198efaedf04374ef451e7aa1fb7f1937712193
aliceout <github.underpay178@slmail.me>
2023-05-29 01:26:34 +0200

    Mise en oeuvre des minimums pour déploiement

--- backend/app/index.js
+++ backend/app/index.js
@@ -4,9 +4,12 @@
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 */
 import dotenv from 'dotenv';                                                  // On récupère les variables d'environnement
-import express from 'express';                                                // On récupère le module Express.js                        
-import { router } from './router.js';                                         // On récupère les param du routeur                     
+import express from 'express';                                                // On récupère le module Express.js
+import { router } from './router.js';                                         // On récupère les param du routeur
 
+import compression from 'compression';
+import helmet from 'helmet';
+import RateLimit from 'express-rate-limit';
 import cors from 'cors';                                             // On récupère le module cors
 
 import { notFound } from './middlewares/index.js';                            // On récupère les middlewaresReload
@@ -26,6 +29,25 @@ const app = express();                                                        //
 ·······  Middlewares
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 */
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+
+// Set up rate limiter: maximum of twenty requests per minute
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+app.use(compression()); // Compress all routes
 app.use(cors('*'));
 
 app.use(express.json());
